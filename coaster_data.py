5cb260f250d9322ee3ac011da073218b990e87f6
Alyssia Bunts <alyssiabunts@gmail.com>
2023-08-22 23:46:17 -0400

    early cleaning process

--- coaster_data.py
+++ coaster_data.py
@@ -4,10 +4,15 @@ import pandas as pd
 stats = pd.read_csv("data\my_coaster_data.csv", header=0)
 # print(stats.head)
 
+# Cleaning
+# TODO: Remove columns for future years
 headers = stats.columns
+headers = stats.rename(columns={'NAME': 'Name'}).drop(columns='<-INSERT HERE')
 print(headers)
 
+
+# Trials
 models = stats.Type.unique()
 # print(models)
 
-print(stats.Park.value_counts())
+# print(stats.Park.value_counts())
